ed0089d09ee0fc2fbc8d4a2fbdc7b3d3885bf71e
CarolinaAT17 <103219728+CarolinaAT17@users.noreply.github.com>
2022-07-04 18:55:12 -0400

    Add files via upload

--- ValidacionRegistro.js
+++ ValidacionRegistro.js
@@ -8,4 +8,43 @@ function validar()
         nombre.focus();
         return;
     }
+
+    //Nombre usuario
+    let usuario = document.getElementById("usuario");
+    let expUsuario = /^[A-Za-zÑñÁÉÍÓÚáéíóú]+(\s[A-Za-zÑñÁÉÍÓÚáéíóú]+)*$/;
+    if(!expUsuario.test(usuario.value)){
+        alert("Solo letras y espacios, máximo 25 caracteres");
+        nombre.focus();
+        return;
+    }
+
+    //Correo Electronico
+
+    //Numero telefonico
+    let telefono = document.getElementById("telefono");
+    let expTelefono = /^\d{9}$/;
+    if(!telefono.value.match(expTelefono)){
+        alert("Movil no valido");
+        movil.focus();
+        return;
+    }
+
+    //RUT
+
+    //Comuna
+
+    //Genero
+
+    //suscripcion
+    let suscripcion = document.getElementsByName("checkbox");
+    let cont = 0;
+    for(let i = 0; i < suscripcion.length; i++){
+        if(suscripcion[i].checked){
+            cont++;
+        }
+    if(cont>3){
+        alert("Debe marcar menos de 3 casillas");
+        return;
+    }
+    }
 }
